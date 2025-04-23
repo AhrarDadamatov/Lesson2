6d5424cedf4e65a1d594ea27fe89376d7c80e222
AuraKazakhstan <info@aura-it.kz>
2025-04-23 13:04:58 +0500

    Lesson2

---
+++ src/Arrays.java
@@ -0,0 +1,12 @@
+import javax.swing.plaf.synth.SynthOptionPaneUI;
+
+public class Arrays {
+    public static void main(String[] args) {
+        int[] number = {1,2,3};
+        int[][] matrice = {{1,2,3,4,5},
+                           {6,7,8,9,14},
+                           {10,11,12,13}};
+        System.out.println(matrice[2][1]);
+
+    }}
+
