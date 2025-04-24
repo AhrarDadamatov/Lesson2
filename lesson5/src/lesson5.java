f9f881a1a7b0c86e8f3793e9fbb1fd5b6c3d3b14
AuraKazakhstan <info@aura-it.kz>
2025-04-24 15:37:47 +0500

    Lesson5

---
+++ lesson5/src/lesson5.java
@@ -0,0 +1,10 @@
+public class lesson5 {
+    public  static void main(String[] args) {
+        final int X = 10;
+        System.out.println(X);
+    }
+class Test{
+        public static final int CONSTANT = 0;
+
+}
+}
