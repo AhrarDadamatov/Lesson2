20fc77fd11c28fc187d28aa7b81ebb23ba646ef0
AuraKazakhstan <info@aura-it.kz>
2025-04-24 11:06:29 +0500

    this

--- lesson3/src/ClassesAndObjects.java
+++ lesson3/src/ClassesAndObjects.java
@@ -1,31 +1,30 @@
 public class ClassesAndObjects {
     public static void main(String[] args) {
-        Person person1 = new Person();
-        person1.name = "Роман";
-        person1.age = 50;
-        Person person2 = new Person();
-        person2.name = "Ануар";
-        person2.age = 30;
-        int year1 = person1.calculateYearsToRetirement();
-        int year2 = person2.calculateYearsToRetirement();
-        System.out.println("Первому человеку до пенсии: "+year1+"лет");
-        System.out.println("Второму человеку до пенсии: "+year1+"лет");
-    }
-}
-class Person{
-    String name;
-    int age;
+        Human human1 = new Human();
+        human1.setAge(18);
+        human1.setName("Tom");
+        human1.getInfo();
 
-    int calculateYearsToRetirement(){
-        int years = 65-age;
-        return years;
     }
+static class Human{
+        String name;
+        int age;
 
-    void speak(){
-        for(int i =0; i<3; i++){
-        System.out.println("Меня зовут "+ name +",мне "+age+" лет");}
-    }
-    void sayHello(){
-        System.out.println("Привет");
-    }
+        public void setName(String name){
+            this.name = name;
+        }
+        public void setAge(int age){
+            this.age = age;
+        }
+        public String getName(){
+            return name;
+        }
+        public int getAge(){
+            return age;
+        }
+        public void getInfo(){
+            System.out.println(name+","+age);
+        }
 }
+}
+
