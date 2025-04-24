dc87fa8003b3b845a0bac80ef926430b88c40bf7
AuraKazakhstan <info@aura-it.kz>
2025-04-24 16:33:49 +0500

    Lesson7

---
+++ lesson7/src/lesson7.java
@@ -0,0 +1,8 @@
+public class lesson7 {
+    public static void main(String [] ags){
+        System.out.printf("String %-10d \n",532);
+        System.out.printf("String %-10d \n",5);
+        System.out.printf("String %-10d \n",415453431);
+        System.out.printf("String %.2f \n",41.225545);
+    }
+}
