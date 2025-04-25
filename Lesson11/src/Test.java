f6c4ef4dac83482d1bc43c4cabc020844d24b54c
AuraKazakhstan <info@aura-it.kz>
2025-04-25 15:18:45 +0500

    lesson10

---
+++ Lesson11/src/Test.java
@@ -0,0 +1,13 @@
+public class Test {
+    public static void main(String[] args){
+        //byte x = 123; //-127 do 127
+        //long y = 123;
+        //double a = 12.54;
+        //float g = 12.2f;
+
+        float f = 123.2f;
+        long l = 1235154546454454L;
+
+
+    }
+}
