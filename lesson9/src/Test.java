de247c0eec450a79307db8ba81070a318f1b465c
AuraKazakhstan <info@aura-it.kz>
2025-04-25 11:17:52 +0500

    lesson9

---
+++ lesson9/src/Test.java
@@ -0,0 +1,13 @@
+import Forest.Squirell;
+import Forest.Treee;
+import Something.SomeClass;
+
+import java.util.Scanner;
+
+public class Test {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        SomeClass saomeInstace = new SomeClass();
+
+    }
+}
