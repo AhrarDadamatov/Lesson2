9f80b1ad11db3ea9d6e8b92174900cac385b2b5c
AuraKazakhstan <info@aura-it.kz>
2025-04-28 16:06:03 +0500

    While

---
+++ while/src/While.java
@@ -0,0 +1,14 @@
+public class While {
+    public static void main(String[] args){
+        int i = 0;
+        while(i <= 10){
+            System.out.println("Element :" + i);
+            i++;
+        }
+        int i = 100;
+        while (i > 5){
+            System.out.println("Element :" + i);
+            i -= 25;
+        }
+    }
+}
