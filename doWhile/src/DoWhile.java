b73cab60c6feb2a07638eb1f502fd02935644673
AuraKazakhstan <info@aura-it.kz>
2025-04-28 16:18:16 +0500

    Do While

---
+++ doWhile/src/DoWhile.java
@@ -0,0 +1,13 @@
+public class DoWhile {
+    public static void main(String[] args){
+        int i = 100;
+        do{
+            System.out.println("Element: " + i);
+        } while (i < 10);
+        int i = 0;
+        do{
+            System.out.println("Element: " + i);
+            i++;
+        } while (i < 10);
+    }
+}
